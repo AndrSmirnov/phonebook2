a97f25e3174a3d9de46116505cc2f305bbc7bb43
AndrSmirnov <asmirnoff.1974@gmail.com>
2024-04-25 07:04:32 +0200

    refactoring to value object

---
+++ src/test/java/config/ApplicationManager.java
@@ -0,0 +1,62 @@
+package config;
+
+import helpers.ContactHelper;
+import helpers.HomePageHelper;
+import helpers.UserHelper;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class ApplicationManager {
+
+    static WebDriver driver;
+    String browser;
+    ContactHelper contactHelper;
+    HomePageHelper homePageHelper;
+    UserHelper userHelper;
+
+    public ContactHelper getContactHelper() {
+        return contactHelper;
+    }
+
+    public HomePageHelper getHomePageHelper() {
+        return homePageHelper;
+    }
+
+    public UserHelper getUserHelper() {
+        return userHelper;
+    }
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
+    public void init() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--lang=en");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(chromeOptions);
+
+        // common settings for the browser
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.navigate().to("https://telranedu.web.app/home");
+
+        // init helpers
+        contactHelper = new ContactHelper(driver);
+        homePageHelper = new HomePageHelper(driver);
+        userHelper = new UserHelper(driver);
+    }
+
+    public void quit() {
+        driver.quit();
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+}
