554082ff77acdce5b0e4fa736b10407ee856048b
AndrSmirnov <asmirnoff.1974@gmail.com>
2024-04-25 07:18:03 +0200

    add app manager

---
+++ src/main/java/leveltwo/AddContactTests.java
@@ -0,0 +1,40 @@
+package leveltwo;
+
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AddContactTests extends BaseTest {
+
+//    @BeforeClass
+//    public void preconditions() {
+//        clickloginOnNavBar();
+//        login(user);
+//    }
+//
+//    @AfterClass
+//    public void methodPostCondition() {
+//        clickLogoutBtn();
+//        navigateToHomePage();
+//    }
+//
+//    @Test
+//    public void positiveAddContact() {
+//        clickAddOnNavBar();
+//        ContactDTO contactDTO = new ContactDTO()
+//                .setName("sjrfbhijs")
+//                .setLastName("jedfnijs")
+//                .setEmail("sjkfhj@mail.com")
+//                .setAddress("ejuhrf")
+//                .setDescription("jkfrnrj")
+//                .setPhone("5554567890");
+//        addContact(contactDTO);
+//
+//        pause(3000);
+//
+//        Assert.assertTrue(isContactDisplaysOnThePage("5554567890"));
+//    }
+
+}
